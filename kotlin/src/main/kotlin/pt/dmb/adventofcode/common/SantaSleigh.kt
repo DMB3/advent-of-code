0f10500f90f998360b82297263cb755cf88d71b6
Diogo Bastos <diogo.bastos@jumo.world>
2025-09-19 14:30:43 +0100

    Added 2016 problem 1

--- kotlin/src/main/kotlin/pt/dmb/adventofcode/common/SantaSleigh.kt
+++ kotlin/src/main/kotlin/pt/dmb/adventofcode/common/SantaSleigh.kt
@@ -1,9 +1,9 @@
 package pt.dmb.adventofcode.common
 
-class SantaSleigh {
+open class SantaSleigh {
 
-    private var facing: FacingDirection = FacingDirection.NORTH
-    private var location: Pair<Int, Int> = Pair(0, 0)
+    protected var facing: FacingDirection = FacingDirection.NORTH
+    protected var location: Pair<Int, Int> = Pair(0, 0)
 
     enum class FacingDirection {
         NORTH, SOUTH, EAST, WEST
@@ -34,7 +34,7 @@ class SantaSleigh {
         }
     }
 
-    fun walk(steps: Int) {
+    open fun walk(steps: Int) {
         location = newCoordinatesOnWalk(facing, steps)
     }
 
@@ -44,6 +44,85 @@ class SantaSleigh {
             FacingDirection.SOUTH -> Pair(location.first, location.second + steps)
             FacingDirection.EAST -> Pair(location.first + steps, location.second)
             FacingDirection.WEST -> Pair(location.first - steps, location.second)
+        }
+    }
+}
+
+class SantaBetterSleigh : SantaSleigh() {
+    private val seen = mutableSetOf<Pair<Int, Int>>()
+    private var shouldStop: Boolean = false
+
+    init {
+        seen.add(location())
+    }
+
+    fun shouldStop() = shouldStop
+
+    override fun walk(steps: Int) {
+        location = newCoordinatesOnWalk(facing, steps)
+    }
+
+    fun addSeenLocation(location: Pair<Int, Int>) {
+        seen.add(location)
+    }
+
+    private fun newCoordinatesOnWalk(direction: FacingDirection, steps: Int): Pair<Int, Int> {
+        return when (direction) {
+            FacingDirection.NORTH -> {
+                for (i in steps downTo 1) {
+                    val newLocation = Pair(location().first, location().second - i)
+                    if (seen.contains(newLocation)) {
+                        shouldStop = true
+                        return newLocation
+                    }
+                    seen.add(newLocation)
+                }
+
+                shouldStop = false
+                return Pair(location().first, location().second - steps)
+            }
+
+            FacingDirection.SOUTH -> {
+                for (i in 1 until steps) {
+                    val newLocation = Pair(location().first, location().second + i)
+                    if (seen.contains(newLocation)) {
+                        shouldStop = true
+                        return newLocation
+                    }
+                    seen.add(newLocation)
+                }
+
+                shouldStop = false
+                return Pair(location().first, location().second + steps)
+            }
+
+            FacingDirection.EAST -> {
+                for (i in 1 until steps) {
+                    val newLocation = Pair(location().first + i, location().second)
+                    if (seen.contains(newLocation)) {
+                        shouldStop = true
+                        return newLocation
+                    }
+                    seen.add(newLocation)
+                }
+
+                shouldStop = false
+                return Pair(location().first + steps, location().second)
+            }
+
+            FacingDirection.WEST -> {
+                for (i in steps downTo 1) {
+                    val newLocation = Pair(location().first - i, location().second)
+                    if (seen.contains(newLocation)) {
+                        shouldStop = true
+                        return newLocation
+                    }
+                    seen.add(newLocation)
+                }
+
+                shouldStop = false
+                return Pair(location().first - steps, location().second)
+            }
         }
     }
 }
--- kotlin/src/test/kotlin/pt/dmb/adventofcode/y2016/p01/PartTwoTest.kt
+++ kotlin/src/test/kotlin/pt/dmb/adventofcode/y2016/p01/PartTwoTest.kt
@@ -2,24 +2,28 @@ package pt.dmb.adventofcode.y2016.p01
 
 import org.junit.jupiter.api.Test
 import pt.dmb.adventofcode.common.Utilities
+import kotlin.test.assertEquals
 
 class PartTwoTest {
     @Test
     fun `Should return correct solution`() {
-        val allResults = mutableListOf<Int>()
-
         Utilities.readInputFile(2016, 1).forEach { line ->
-            val partOne = PartOne()
+            val partTwo = PartTwo()
 
-            TODO()
+            line.split(",").forEach { instruction ->
+                val direction = instruction.trim()[0]
+                val amount = instruction.trim().substring(1).toInt()
+
+                partTwo.sleigh.turn(direction)
+                partTwo.sleigh.walk(amount)
+                if (partTwo.sleigh.shouldStop()) {
+                    assertEquals(Pair(4, 0), partTwo.sleigh.location())
+                    assertEquals(4, partTwo.distance())
+                    return
+                }
+
+                partTwo.sleigh.addSeenLocation(partTwo.sleigh.location())
+            }
         }
     }
 }
-
-/*
-R2, L3
-R2, R2, R2
-R5, L5, R5, R3
-R8, R4, R4, R8
-Repeated at (4, 0) which is 4 steps away
- */
