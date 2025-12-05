a60a7d15ddef75d8a1248c68ce854aedc508d2fb
Diogo Bastos <diogo.bastos@jumo.world>
2025-12-05 10:05:38 +0000

    Added day 1 of 2025

---
+++ 2025/001/part_one.py
@@ -0,0 +1,41 @@
+import common
+
+class Dial(object):
+    def __init__(self, initial_value = 50, minimum_value = 0, maximum_value = 99):
+        self.value = initial_value
+        self.minimum_value = minimum_value
+        self.maximum_value = maximum_value
+
+    def __rotate_left(self):
+        self.value -= 1
+        if self.value < self.minimum_value:
+            self.value = self.maximum_value
+
+    def __rotate_right(self):
+        self.value += 1
+        if self.value > self.maximum_value:
+            self.value = self.minimum_value
+
+    def rotate(self, direction, times):
+        if direction == 'L':
+            for n in range(times):
+                self.__rotate_left()
+        elif direction == 'R':
+            for n in range(times):
+                self.__rotate_right()
+
+if __name__ == "__main__":
+    for input_file in common.inputs:
+        print(input_file)
+        zero_times = 0
+        dial = Dial()
+
+        with open(input_file) as fin:
+            for line in fin.readlines():
+                direction, amount = line[0], int(line[1:].strip())
+                dial.rotate(direction, amount)
+
+                if dial.value == 0:
+                    zero_times += 1
+
+        print(zero_times)
---
+++ 2025/001/readme.md
@@ -0,0 +1,6 @@
+Secret Entrance
+===============
+
+https://adventofcode.com/2025/day/1
+
+Basically a brute force solution...
